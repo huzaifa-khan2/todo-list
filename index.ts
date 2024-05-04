ea3e237fd749801dbf23745fda2fa712136e24ea
huzaifa khan <hk4899541@gmail.com>
2024-05-05 01:53:40 +0500

    all files uploaded

---
+++ index.ts
@@ -0,0 +1,31 @@
+import inquirer from "inquirer";
+import chalk from "chalk";
+
+let todolist = [];
+let condition = true;
+console.log(chalk.magenta.bold("\n \t  Welcome to CodeWithHuzaifa - Todo List Application\n"));
+
+while (condition) {
+  let addTask = await inquirer.prompt([
+    {
+      name: "task",
+      type: "input",
+      message: chalk.green.bold("Enter your new Task:"),
+    },
+  ]);
+  todolist.push(addTask.task);
+  console.log(`${addTask.task} Task Added In Todo List Successfully`);
+
+  let addmoretask = await inquirer.prompt([
+    {
+      name: "AddMore",
+      type: "confirm",
+      message: "Do you want to ADD more Task?",
+      default: "false",
+    },
+  ]);
+
+  condition = addmoretask.AddMore
+}
+
+console.log("Your updated todo list: "  , todolist);
